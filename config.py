e7d5d92494b91accbcc88a6c90a20c6afb154256
deepdewdeep <deependradewagiri@gmail.com>
2025-02-23 19:30:40 +0530

    pure yaml

--- config.py
+++ config.py
@@ -353,7 +353,7 @@ _C.NUM_OF_GPU_TRAIN = 1
 # Log settings
 # -----------------------------------------------------------------------------
 _C.LOG = CN()
-_C.LOG.PATH = "runs/exp"
+_C.LOG.PATH = "/content/drive/MyDrive/DDCache/PhysMambaSF_SP/runs/exp"
 
 
 def _update_config_from_file(config, cfg_file):
